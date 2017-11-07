4fd979cbe9d40d5c10949e74a978a39570c0f6b6
Jordan Melberg <jordan.melberg@okta.com>
2017-11-07 09:00:06 -0800

    :art: Redesign /documentation page to help browser shrinkage (#1460)
    Resolves: OKTA-147653

--- _source/_assets/js/docsIndex.js
+++ _source/_assets/js/docsIndex.js
@@ -1,39 +1,52 @@
 (function($) {
   // Handle building the icons and code languages
-  var code = [
+  var frontend = [
+    { name: 'android', label: 'Android' },
     { name: 'angular', label: 'Angular' },
     { name: 'react',label: 'React' },
     { name: 'javascript', label: 'JavaScript' },
-    { name: 'android', label: 'Android' },
     { name: 'ios',  label: 'iOS' },
-    { name: 'nodejs', label: 'Node.js' },
+  ]
+
+  var server = [
     { name: 'java', label: 'Java' },
     { name: 'dotnet', label: '.NET' },
+    { name: 'nodejs', label: 'Node.js' },
     { name: 'php', label: 'PHP' },
     { name: 'python', label: 'Python' }
   ]
 
-  function renderCodeLinks() {
-    var ul = $('<ul>', {});
-    code.forEach(function (language) {
-      var li = $('<li>');
-
+  function renderCodeLinks(codeLanguages) {
+    var row = $('<tr class="docsPageTableRow">');
+    codeLanguages.forEach(function(language) {
+      var td = $('<td class="docsPageTableData">');
       var icon = $('<i>', {
-        class: 'icon code-' + language.name + '-32',
+        class: 'icon docsPage code-' + language.name + '-32',
       });
 
       var link = $('<a>', {
-        text: language.label + ' ›',
+        text: language.label,
         href: '/code/' + language.name + '/',
       });
       link.prepend($('<br />'));
       link.prepend(icon);
-      li.append(link);
-      ul.append(li);
+      td.append(link);
+      row.append(td);
     });
-    $('#docs-languages').append(ul);
+    return row;
+  }
+
+  function renderCodeRows() {
+    var table = $('<table class="docsPageTable">');  
+    // Build first row of frontend languages
+    table.append(renderCodeLinks(frontend));
+
+    // Build second row of server languages
+    table.append(renderCodeLinks(server));
+
+    $('#docs-languages').append(table);    
   }
 
   // Load documentation icons
-  renderCodeLinks();
+  renderCodeRows();
 })(jQuery);
