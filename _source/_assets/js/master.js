ac9dd81aab89818462d51280de8945a3d531971f
Tom Risse <tom@flickerbox.com>
2018-02-06 15:30:45 -0800

    Remove JS references to Primary Nav, hover now applied in master.css from okta.com (#1726)

--- _source/_assets/js/master.js
+++ _source/_assets/js/master.js
@@ -66,15 +66,6 @@ function oktaCustomRenderFunction(document_type, item) {
     $('.Page').toggleClass('PrimaryNav-is-active');
   });
 
-  $('.PrimaryNav .expanded .nolink, .PrimaryNav .expanded > a').hover(function(event){
-	$('.PrimaryNav .expanded').removeClass('is-active');
-	if ($(this).hasClass('nolink')) {
-		event.stopPropagation();
-		event.preventDefault();
-	}
-	$(this).parent('li').addClass('is-active');
-  });
-
 
   $('#form_search #st-search-input-auto').on('keyup', function(){
       if ($(this).val().length > 0) {
@@ -101,7 +92,6 @@ function oktaCustomRenderFunction(document_type, item) {
 
   $(window).on('click', function() {
     $('.search-active').removeClass('search-active');
-    $('.PrimaryNav .expanded.is-active').removeClass('is-active');
   });
 
   $(window).on('resize', function(e){
