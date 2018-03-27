aefe8d70b46c7941499f256c3a90818698b6b2be
Travis CI User <travis@example.org>
2018-03-27 18:41:52 +0000

    Deploy site for 4f5f4c814eae5a94154e956f08c9a487cf838331

---
+++ assets/docsIndex-451037ff8962767800ef6cf33c5651d17051f09fbe37884d0850e574e4177191.js
@@ -0,0 +1,36 @@
+(function($) {
+  // Handle building the icons and code languages
+  var codeLanguages = [
+    { name: 'android', label: 'Android' },
+    { name: 'angular', label: 'Angular' },
+    { name: 'react',label: 'React' },
+    { name: 'ios',  label: 'iOS' },
+    { name: 'javascript', label: 'JavaScript' },
+    { name: 'java', label: 'Java' },
+    { name: 'dotnet', label: '.NET' },
+    { name: 'nodejs', label: 'Node.js' },
+    { name: 'php', label: 'PHP' },
+    { name: 'python', label: 'Python' }
+  ];
+
+  function renderCodeLinks() {
+    var flexBox = $('<div class="docs-page-tiles">');
+    codeLanguages.forEach(function(language) {
+      var icon = $('<i>', {
+        class: 'icon docsPage code-' + language.name + '-32',
+      });
+      var link = $('<a>', {
+        text: language.label,
+        href: '/code/' + language.name + '/',
+      });
+      link.prepend($('<br />'));
+      link.prepend(icon);
+      flexBox.append(link);
+    });
+
+    $('#docs-languages').append(flexBox);
+  }
+
+  // Load documentation icons
+  renderCodeLinks();
+})(jQuery);
