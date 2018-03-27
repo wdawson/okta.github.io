4f5f4c814eae5a94154e956f08c9a487cf838331
Alex Dahl <alex.dahl@okta.com>
2018-03-27 08:22:09 -1000

    style tweaks and fixes for docs front page (#1895)

--- _source/_assets/js/docsIndex.js
+++ _source/_assets/js/docsIndex.js
@@ -1,52 +1,36 @@
 (function($) {
   // Handle building the icons and code languages
-  var frontend = [
+  var codeLanguages = [
     { name: 'android', label: 'Android' },
     { name: 'angular', label: 'Angular' },
     { name: 'react',label: 'React' },
-    { name: 'javascript', label: 'JavaScript' },
     { name: 'ios',  label: 'iOS' },
-  ]
-
-  var server = [
+    { name: 'javascript', label: 'JavaScript' },
     { name: 'java', label: 'Java' },
     { name: 'dotnet', label: '.NET' },
     { name: 'nodejs', label: 'Node.js' },
     { name: 'php', label: 'PHP' },
     { name: 'python', label: 'Python' }
-  ]
+  ];
 
-  function renderCodeLinks(codeLanguages) {
-    var row = $('<tr class="docsPageTableRow">');
+  function renderCodeLinks() {
+    var flexBox = $('<div class="docs-page-tiles">');
     codeLanguages.forEach(function(language) {
-      var td = $('<td class="docsPageTableData">');
       var icon = $('<i>', {
         class: 'icon docsPage code-' + language.name + '-32',
       });
-
       var link = $('<a>', {
         text: language.label,
         href: '/code/' + language.name + '/',
       });
       link.prepend($('<br />'));
       link.prepend(icon);
-      td.append(link);
-      row.append(td);
+      flexBox.append(link);
     });
-    return row;
-  }
 
-  function renderCodeRows() {
-    var table = $('<table class="docsPageTable">');  
-    // Build first row of frontend languages
-    table.append(renderCodeLinks(frontend));
-
-    // Build second row of server languages
-    table.append(renderCodeLinks(server));
-
-    $('#docs-languages').append(table);    
+    $('#docs-languages').append(flexBox);
   }
 
   // Load documentation icons
-  renderCodeRows();
+  renderCodeLinks();
 })(jQuery);
