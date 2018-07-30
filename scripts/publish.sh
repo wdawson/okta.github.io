351eab91e9d2854159ba9b4bdfd560f2f33111af
David Joyce <david.joyce@okta.com>
2018-07-30 17:17:31 -0400

    General script cleanup

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -vx
+#!/bin/bash
 
 DEPLOY_BRANCH="weekly"
 DEPLOY_ENVIRONMENT=""
@@ -28,7 +28,7 @@ interject "Building HTML in $(pwd)"
 if ! generate_html;
 then
     echo "Error building site"
-    exit ${BUILD_FAILURE};
+    exit ${BUILD_FAILURE}
 fi
 
 # Run markdown lint checker
@@ -48,24 +48,21 @@ fi
 # Check if we are in one of our publish branches
 if [[ -z "${branch_environment_map[$BRANCH]+unset}" ]]; then
     echo "Current branch is not a publish branch"
-    exit $SUCCESS
+    exit ${SUCCESS}
 else
     DEPLOY_ENVIRONMENT=${branch_environment_map[$BRANCH]}
 fi
 
 interject "Generating conductor file in $(pwd)"
-if ! generate_conductor_file;
-then
+if ! generate_conductor_file; then
     echo "Error generating conductor file"
-    exit ${BUILD_FAILURE};
+    exit ${BUILD_FAILURE}
 fi
 
 # ----- Start (Temporary) Deploy to S3 -----
-if [[ "${BRANCH}" == "${DEPLOY_BRANCH}" ]];
-then
+if [[ "${BRANCH}" == "${DEPLOY_BRANCH}" ]]; then
     interject "Uploading HTML from '${GENERATED_SITE_LOCATION}' to '${TARGET_S3_BUCKET}'"
-    if ! aws s3 cp ${GENERATED_SITE_LOCATION} ${TARGET_S3_BUCKET} --recursive;
-    then
+    if ! aws s3 cp ${GENERATED_SITE_LOCATION} ${TARGET_S3_BUCKET} --recursive; then
         echo "Error uploading HTML to S3"
         exit ${BUILD_FAILURE}
     fi
@@ -74,48 +71,47 @@ fi
 
 # Create NPM package
 # ------------------
-if [ -n "$action_branch" ];
-then
-  echo "Publishing from bacon task using branch $action_branch"
-  TARGET_BRANCH=$action_branch
+if [ -n "$action_branch" ]; then
+  echo "Publishing from bacon task using branch ${action_branch}"
+  TARGET_BRANCH=${action_branch}
 else
-  echo "Publishing from bacon testSuite using branch $BRANCH"
-  TARGET_BRANCH=$BRANCH
+  echo "Publishing from bacon testSuite using branch ${BRANCH}"
+  TARGET_BRANCH=${BRANCH}
 fi
 
 if ! ci-update-package --branch ${TARGET_BRANCH}; then
   echo "ci-update-package failed! Exiting..."
-  exit $FAILED_SETUP
+  exit ${FAILED_SETUP}
 fi
 
 if ! npm publish --registry ${REGISTRY}; then
   echo "npm publish failed! Exiting..."
-  exit $PUBLISH_ARTIFACTORY_FAILURE
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
 DATALOAD=$(ci-pkginfo -t dataload)
 if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
   echo "artifactory_curl failed! Exiting..."
-  exit $PUBLISH_ARTIFACTORY_FAILURE
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
-ARTIFACT_FILE="$([[ $DATALOAD =~ okta\.github\.io-(.*)\.tgz ]] && echo $BASH_REMATCH)"
-DEPLOY_VERSION="$([[ $ARTIFACT_FILE =~ okta\.github\.io-(.*)\.tgz ]] && echo ${BASH_REMATCH[1]})"
+ARTIFACT_FILE="$([[ ${DATALOAD} =~ okta\.github\.io-(.*)\.tgz ]] && echo ${BASH_REMATCH})"
+DEPLOY_VERSION="$([[ ${ARTIFACT_FILE} =~ okta\.github\.io-(.*)\.tgz ]] && echo ${BASH_REMATCH[1]})"
 ARTIFACT="@okta/okta.github.io/-/@okta/${ARTIFACT_FILE}"
 
-if ! send_promotion_message "${DEPLOY_ENVIRONMENT}" "${ARTIFACT}" "${DEPLOY_VERSION}" ; then
+if ! send_promotion_message "${DEPLOY_ENVIRONMENT}" "${ARTIFACT}" "${DEPLOY_VERSION}"; then
   echo "Error sending promotion event to aperture"
-  exit ${BUILD_FAILURE};
+  exit ${BUILD_FAILURE}
 fi
 
 # Trigger Runscope tests
-if [[ "${BRANCH}" == "${DEPLOY_BRANCH}" ]];
-then
-    STAGING_BASE_URL_RUNSCOPE="https://developer.trexcloud.com";
+if [[ "${BRANCH}" == "${DEPLOY_BRANCH}" ]]; then
+    STAGING_BASE_URL_RUNSCOPE="https://developer.trexcloud.com"
 else
-    STAGING_BASE_URL_RUNSCOPE="https://developer.okta.com";
+    STAGING_BASE_URL_RUNSCOPE="https://developer.okta.com"
 fi
 
-curl -I -X GET "https://api.runscope.com/radar/bucket/$RUNSCOPE_TRIGGER_ID/trigger?base_url=$STAGING_BASE_URL_RUNSCOPE"
+curl -I -X GET "https://api.runscope.com/radar/bucket/${RUNSCOPE_TRIGGER_ID}/trigger?base_url=${STAGING_BASE_URL_RUNSCOPE}"
 
-exit $SUCCESS
+exit ${SUCCESS}
+
